06cb895614e68229c2d32a132319dba1b8b10a7b
diegochamo2109 <demachamo@gmail.com>
2025-08-13 10:31:56 -0300

    "login y inicio"
    Signed-off-by: diegochamo2109 <demachamo@gmail.com>

--- romacontrol_v1/src/main/java/com/romacontrol/romacontrol_v1/config/WebConfig.java
+++ romacontrol_v1/src/main/java/com/romacontrol/romacontrol_v1/config/WebConfig.java
@@ -1,19 +1,21 @@
 package com.romacontrol.romacontrol_v1.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
   @Override
-  public void addCorsMappings(@NonNull CorsRegistry registry) {
+  public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("*") // en prod: especificar tu dominio
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        .allowedHeaders("*");
-        // .allowCredentials(true) // si luego usás cookies/autenticación, configura origins específicos
+      .allowedOrigins(
+        "http://127.0.0.1:5500",   // front Live Server
+        "http://localhost:5500"    // opcional, por si a veces usás localhost
+      )
+      .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+      .allowedHeaders("*")
+      .allowCredentials(true)
+      .maxAge(3600);
   }
 }
