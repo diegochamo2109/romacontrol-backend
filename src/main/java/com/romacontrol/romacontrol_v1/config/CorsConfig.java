8058a08cdb39ccd4cff585768b2d7fc2a265e187
diegochamo2109 <demachamo@gmail.com>
2025-11-21 18:37:15 -0300

    Fix CORS for Netlify

---
+++ src/main/java/com/romacontrol/romacontrol_v1/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.romacontrol.romacontrol_v1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:5500",
+                            "http://127.0.0.1:5500",
+                            "http://localhost:8080",
+                            "https://voluble-kulfi-9c5ef1.netlify.app",
+                            "https://*.netlify.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
